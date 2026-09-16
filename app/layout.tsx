3a1f947a7c080eb4e9f72d1fa2d96822d6d6176b
hamza <hamzanasim248@gmail.com>
2026-09-16 20:28:25 +0500

    Add Clarity tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,17 @@ export default function RootLayout({
       lang="en"
       className={`${openSans.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yj6kjjgcgn");`,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col bg-white text-ink font-sans" suppressHydrationWarning>
         <Header />
         <main className="flex-1">{children}</main>
